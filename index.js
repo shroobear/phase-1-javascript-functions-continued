56f6524f6499efe3b55565ee6c665a8a8f5258c3
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-03-15 16:15:20 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const saturdayFun = function(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(toDo='go to the office') {
+    return `This Monday, I will ${toDo}.`
+}
+
+const wrapAdjective = function(emphasis) {
+    return function (compliment='special') {
+        return `You are ${emphasis}${compliment}${emphasis}!`
+    }
+};
